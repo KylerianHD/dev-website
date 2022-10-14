4c5063ab5055c5a1455ba2a00745737e550a5107
KylerianHD <anders.jon@web.de>
2022-10-14 21:15:48 +0200

    Created basic html, css and js (Light- & Darkmode)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+function LightMode() {
+    var element = document.body;
+    element.className = "light-mode";
+}
 
+function DarkMode() {
+    var element = document.body;
+    element.className = "dark-mode";
+}
